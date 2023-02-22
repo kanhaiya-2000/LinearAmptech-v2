ba22d9c816d10afa054f9d50e074115189d6163d
Omkar0803 <omkarbdarde@gmail.com>
2023-02-23 01:39:30 +0530

    first commit

---
+++ src/Pages/AboutUs.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Footer, Header } from "../Components";
+
+const AboutUs = () => {
+  return (
+    <div>
+      <Header />
+      <Footer />
+    </div>
+  );
+};
+
+export default AboutUs;
