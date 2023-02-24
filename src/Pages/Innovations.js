636790012869c6b9040631b4af9048f893292ffc
Hafizur Rahman <99hafizurrahman@gmail.com>
2023-02-25 01:04:14 +0530

    icon change

--- src/Pages/Innovations.js
+++ src/Pages/Innovations.js
@@ -582,16 +582,16 @@ const Innovations = () => {
                       <span className="inline-block p-3  text-[#0465F8] bg-blue-100    dark:bg-blue-500">
                         <svg
                           xmlns="http://www.w3.org/2000/svg"
-                          className="w-6 h-6"
                           fill="none"
                           viewBox="0 0 24 24"
+                          strokeWidth={2}
                           stroke="currentColor"
+                          className="w-6 h-6"
                         >
                           <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            stroke-width="2"
-                            d="M5 3v4M3 5h4M6 17v4m-2-2h4m5-16l2.286 6.857L21 12l-5.714 2.143L13 21l-2.286-6.857L5 12l5.714-2.143L13 3z"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M3.75 13.5l10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75z"
                           />
                         </svg>
                       </span>
@@ -630,16 +630,16 @@ const Innovations = () => {
                       <span className="inline-block p-3  text-[#0465F8] bg-blue-100    dark:bg-blue-500">
                         <svg
                           xmlns="http://www.w3.org/2000/svg"
-                          className="w-6 h-6"
                           fill="none"
                           viewBox="0 0 24 24"
+                          strokeWidth={1.5}
                           stroke="currentColor"
+                          className="w-6 h-6"
                         >
                           <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            stroke-width="2"
-                            d="M5 3v4M3 5h4M6 17v4m-2-2h4m5-16l2.286 6.857L21 12l-5.714 2.143L13 21l-2.286-6.857L5 12l5.714-2.143L13 3z"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M4.5 12a7.5 7.5 0 0015 0m-15 0a7.5 7.5 0 1115 0m-15 0H3m16.5 0H21m-1.5 0H12m-8.457 3.077l1.41-.513m14.095-5.13l1.41-.513M5.106 17.785l1.15-.964m11.49-9.642l1.149-.964M7.501 19.795l.75-1.3m7.5-12.99l.75-1.3m-6.063 16.658l.26-1.477m2.605-14.772l.26-1.477m0 17.726l-.26-1.477M10.698 4.614l-.26-1.477M16.5 19.794l-.75-1.299M7.5 4.205L12 12m6.894 5.785l-1.149-.964M6.256 7.178l-1.15-.964m15.352 8.864l-1.41-.513M4.954 9.435l-1.41-.514M12.002 12l-3.75 6.495"
                           />
                         </svg>
                       </span>
