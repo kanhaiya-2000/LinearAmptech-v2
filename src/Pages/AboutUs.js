6c4a01650a22653db1ef5bc0a85f4493e49c9e84
Hafizur Rahman <99hafizurrahman@gmail.com>
2023-03-09 18:49:41 +0530

    update about us page

--- src/Pages/AboutUs.js
+++ src/Pages/AboutUs.js
@@ -7,47 +7,10 @@ import aboutbg from "../assets/aboutbg.jpg";
 import rf from "../assets/rfpower/0.png";
 import sdr from "../assets/SDR/1.jpg";
 import poweramp from "../assets/PowerAmp/1.png";
-import karun from "../assets/profile/karun.png";
-import minakshi from "../assets/profile/minakshi.png";
-import vivek from "../assets/profile/vivek.jpeg";
-import gowrish from "../assets/profile/gowrish.jpg";
+
 import BoardMember from "../Components/BoardMember";
 
-const boardMember = [
-  {
-    photo: karun,
-    name: "Dr. Karun Rawat",
-    position: "Founder & Chairman",
-    linkedin: "https://www.linkedin.com/in/karun-rawat-b732784b/",
-    twitter: "#",
-    facebook: "#",
-  },
-  {
-    photo: minakshi,
-    name: "Dr. Minakshi Rawat",
-    position: "Founder & Director",
-    linkedin: "#",
-    twitter: "#",
-    facebook: "#",
-  },
-
-  {
-    photo: gowrish,
-    name: "Dr. Gowrish B",
-    position: "Director",
-    linkedin: "#",
-    twitter: "#",
-    facebook: "#",
-  },
-  {
-    photo: vivek,
-    name: "Mr. Vivek Sharma",
-    position: "Director",
-    linkedin: "https://www.linkedin.com/in/vivek-sharma-986950121/",
-    twitter: "#",
-    facebook: "#",
-  },
-];
+import { boardMember } from "../data/BoardMemberData";
 
 const AboutUs = () => {
   return (
