d642befa6a9a82ec44bbe0a21daf443751c1334c
Hafizur Rahman <99hafizurrahman@gmail.com>
2023-03-13 23:48:57 +0530

    product page

--- src/Pages/products/CyberSystem.js
+++ src/Pages/products/CyberSystem.js
@@ -3,6 +3,7 @@ import { BgElement2, BgElement1, BgElement3 } from "../../assets";
 import { Footer, Header } from "../../Components";
 import AOS from "aos";
 import "aos/dist/aos.css";
+import foucusImg from "../../assets/product-focus-bg.png";
 
 const CyberSystem = () => {
   useEffect(() => {
@@ -12,9 +13,9 @@ const CyberSystem = () => {
     <div>
       <Header />
       <div className="w-[100%] h-[100%]">
-        <div className="absolute w-[100%] h-[100%] top-[35%]">
+        <div className="absolute w-[100%] h-[100%] lg:top-[35%] md:top-[20%] top-[18%]">
           <h2
-            className="text-[5rem] font-bold font-Roboto drop-shadow-2xl text-center text-white "
+            className="lg:text-[5rem] md:text-6xl text-3xl font-bold font-Roboto drop-shadow-2xl text-center text-white  "
             data-aos="fade"
             data-aos-duration="1000"
           >
@@ -24,7 +25,7 @@ const CyberSystem = () => {
         </div>
         <img src={require("../../assets/bg.png")} className="w-[100%]"></img>
       </div>
-      <div className="text-[1.3rem] leading-relaxed px-[15%] text-center font-medium">
+      <div className="text-[1.3rem] leading-relaxed lg:px-[15%] px-6 text-center font-medium">
         <h2
           className="text-[2.5rem] text-center font-bold mt-5 mb-10"
           data-aos="fade-up"
@@ -69,15 +70,15 @@ const CyberSystem = () => {
       >
         <BgElement1 className="w-[200px] absolute origin-center -right-20 -translate-y-[20%]" />
       </div>
-      <div className="productContainer  w-[100%] text-[1.2rem] leading-relaxed">
-        <div className="flex  justify-center gap-16 my-36 px-[10%]">
+      <div className="productContainer  w-[100%] text-[1.2rem] leading-relaxed ">
+        <div className="lg:flex  justify-center gap-16 my-36 px-[10%]">
           <div className="w-[400px] h-[400px] bg-[#F2F2F2] rounded-2xl shadow-md">
             {/* <img
               src={require("../../assets/RFlinear/p1.png")}
               className="w-[100%]"
             ></img> */}
           </div>
-          <div className="w-[800px] flex flex-col justify-between">
+          <div className="lg:w-[800px] max-w-[500px] flex lg:flex-col  justify-between">
             <div>
               <h2 className="text-[2rem] font-bold mb-5" data-aos="fade-up">
                 WAIT System
@@ -102,8 +103,8 @@ const CyberSystem = () => {
           </div>
         </div>
 
-        <div className="flex  justify-center gap-16 py-40 px-[10%] text-white bg-primary-color">
-          <div className="w-[800px] flex flex-col justify-between">
+        <div className="lg:flex  justify-center gap-16 py-40 px-[10%] text-white bg-primary-color">
+          <div className="lg:w-[800px] max-w-[500px] flex flex-col justify-between">
             <div>
               <h2 className="text-[2rem] font-bold mb-5" data-aos="fade-up">
                 TRACK System
@@ -136,14 +137,14 @@ const CyberSystem = () => {
           </div>
         </div>
 
-        <div className="flex  justify-center gap-16 my-40 px-[10%]">
+        <div className="lg:flex  justify-center gap-16 my-40 px-[10%]">
           <div className="w-[400px] h-[400px] bg-[#F2F2F2] rounded-2xl flex justify-center items-center shadow-md">
             {/* <img
               src={require("../../assets/RFlinear/p3.png")}
               className="w-[90%]"
             ></img> */}
           </div>
-          <div className="w-[800px] flex flex-col justify-between">
+          <div className="lg:w-[800px] max-w-[500px] flex flex-col justify-between">
             <div>
               <h2 className="text-[2rem] font-bold mb-5" data-aos="fade-up">
                 PLAN System
@@ -173,50 +174,50 @@ const CyberSystem = () => {
         </div>
       </div>
       <div className="text-white text-[1.3rem] leading-relaxed">
-        <div className="absolute py-10 px-[12%] ">
-          <h2
-            className="text-[3rem] font-bold text-center mb-10"
-            data-aos="fade-up"
-          >
-            What we focus
-          </h2>
-          <p className="">
-            Linear Amplifier Technology & Services private ltd (Linear Amp-tech)
-            is a company formed and driven by innovative minds of Indian I.I.T
-            Roorkee. The company is Incubated in I.I.T Roorkee and includes
-            students and professors on the board of directors.
-            <br />
-            <br /> The company is focused on innovation to cater to various
-            technical challenges in the area of Electronics and Software
-            Engineering. The company is geared up to take on new technology
-            development projects in the relevant areas. The company is primarily
-            focusing on various aspects of cyber-physical system design
-            including radio-frequency sensors development and addressing the
-            cyber domain with Artificial intelligence and Machine Learning. The
-            company also envisages the requirement for the specialized
-            technology areas such as Radio-frequency design and hence is
-            committed to the development of radios and related components to
-            address the immediate needs of country’s self-reliance drive.
-            <br />
-            <br /> The vision is to foster R&D into product design and
-            development competing with cutting-edge technologies for the
-            national and international market. The R&D is backed by the know-how
-            generated from academics and is therefore capable of embarking new
-            and advanced technology into products. <br />
-            <br />
-            We are dedicated to exploring new technologies in our core product
-            areas and have established a good ecosystem between academics and
-            industry for technology development with innovation. We have a
-            strong team with I.I.T background who are continuously innovating
-            new technology solutions for the company. We believe in continuous
-            innovation for bringing cutting-edge technologies to the market.
-          </p>
-        </div>
-        <div className="h-[800px] overflow-hidden ">
-          <img
-            src={require("../../assets/product-focus-bg.png")}
-            className="w-[100%]"
-          ></img>
+        <div
+          className="text-white text-[1.3rem] leading-relaxed"
+          style={{ backgroundImage: `url(${foucusImg})` }}
+        >
+          <div className=" py-10 px-[12%] ">
+            <h2
+              className="text-[3rem] font-bold text-center mb-10"
+              data-aos="fade-up"
+            >
+              What we focus
+            </h2>
+            <p className="">
+              Linear Amplifier Technology & Services private ltd (Linear
+              Amp-tech) is a company formed and driven by innovative minds of
+              Indian I.I.T Roorkee. The company is Incubated in I.I.T Roorkee
+              and includes students and professors on the board of directors.
+              <br />
+              <br /> The company is focused on innovation to cater to various
+              technical challenges in the area of Electronics and Software
+              Engineering. The company is geared up to take on new technology
+              development projects in the relevant areas. The company is
+              primarily focusing on various aspects of cyber-physical system
+              design including radio-frequency sensors development and
+              addressing the cyber domain with Artificial intelligence and
+              Machine Learning. The company also envisages the requirement for
+              the specialized technology areas such as Radio-frequency design
+              and hence is committed to the development of radios and related
+              components to address the immediate needs of country’s
+              self-reliance drive.
+              <br />
+              <br /> The vision is to foster R&D into product design and
+              development competing with cutting-edge technologies for the
+              national and international market. The R&D is backed by the
+              know-how generated from academics and is therefore capable of
+              embarking new and advanced technology into products. <br />
+              <br />
+              We are dedicated to exploring new technologies in our core product
+              areas and have established a good ecosystem between academics and
+              industry for technology development with innovation. We have a
+              strong team with I.I.T background who are continuously innovating
+              new technology solutions for the company. We believe in continuous
+              innovation for bringing cutting-edge technologies to the market.
+            </p>
+          </div>
         </div>
       </div>
       <Footer />
