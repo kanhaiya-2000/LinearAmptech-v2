42131f3554a6bcb22d5031508068ace5b28bdf60
Hafizur Rahman <99hafizurrahman@gmail.com>
2023-03-14 03:52:00 +0530

    lamp2 add

--- src/Pages/products/RFAmplifier/LAMP2.js
+++ src/Pages/products/RFAmplifier/LAMP2.js
@@ -18,7 +18,7 @@ const LAMP2 = () => {
         <p className="font-semibold text-[1.5rem]">
           50 Ω, Wideband, 1.5 to 2.5 GHz, 7.7-10 W, 28V, SMA.
         </p>
-        <div className="flex justify-between">
+        <div className="lg:flex justify-between">
           <div className="w-[50%]">
             <h2 className="text-[1.8rem] font-bold mt-5">Key Features</h2>
             <ul className="list-disc my-3 ml-8 flex flex-col gap-1">
@@ -38,9 +38,9 @@ const LAMP2 = () => {
               <li> communication systems</li>
             </ul>
           </div>
-          <div className="rightBox w-[50%] flex p-20">
-            <div className="fronBox h-[400px] w-[500px] bg-primary-color"></div>
-            <div className="backBox absolute h-[400px] w-[500px] bg-[#EFF6FF] translate-x-5 translate-y-5 flex justify-center items-center">
+          <div className="rightBox h-[500px] lg:w-[50%] flex lg:p-20">
+            <div className="fronBox lg:h-[400px] lg:w-[500px] md:h-[300px] md:w-[400px] w-[340px] h-[240px] bg-primary-color"></div>
+            <div className="backBox absolute lg:h-[400px] lg:w-[500px] md:h-[300px] md:w-[400px] w-[340px] h-[240px]    bg-[#EFF6FF] lg:translate-x-5 lg:translate-y-5 md:translate-x-3 translate-x-2 translate-y-2 md:translate-y-3 flex justify-center items-center">
               <img src={require("../../../assets/rfpower/PAMOCBJ.png")}></img>
             </div>
           </div>
@@ -49,21 +49,23 @@ const LAMP2 = () => {
           <BgElement2 className="w-[300px] absolute origin-center -left-44 -translate-y-[30%]" />
         </div>
         <div className="text-[1.8rem] font-bold mt-5 mb-10">Diagrams</div>
-        <div className="imgContainer flex flex-col gap-24">
-          <div className="flex gap-20 justify-center">
-            <div className="w-[550px] h-[500px] flex justify-center items-center">
-              <img src={require("../../../assets/rfpower/PAMOD1.png")}></img>
+
+        <div className="imgContainer flex flex-col  lg:gap-24 gap-12">
+          <div className="flex lg:flex-row md:flex-row sm:flex-row lg:gap-20 md:gap-12 sm:gap-10 gap-5 flex-col justify-center">
+            <div className="max-w-[550px] flex justify-center items-center">
+              <img src={require("../../../assets/rfpower/PAMCD1.png")}></img>
             </div>
-            <div className="w-[550px] flex justify-center items-center">
-              <img src={require("../../../assets/rfpower/PAMOD4.png")}></img>
+            <div className="max-w-[550px] flex justify-center items-center">
+              <img src={require("../../../assets/rfpower/PAMCD4.png")}></img>
             </div>
           </div>
-          <div className="flex gap-20 justify-center">
-            <div className="w-[550px] flex justify-center items-center">
-              <img src={require("../../../assets/rfpower/PAMOD2.png")}></img>
+
+          <div className="flex lg:flex-row md:flex-row sm:flex-row lg:gap-20 md:gap-12 sm:gap-10 gap-5 flex-col justify-center">
+            <div className="max-w-[550px] flex justify-center items-center">
+              <img src={require("../../../assets/rfpower/PAMCD2.png")}></img>
             </div>
-            <div className="w-[550px] flex justify-center items-center">
-              <img src={require("../../../assets/rfpower/PAMOD3.png")}></img>
+            <div className="max-w-[550px] flex justify-center items-center">
+              <img src={require("../../../assets/rfpower/PAMCD3.png")}></img>
             </div>
           </div>
         </div>
